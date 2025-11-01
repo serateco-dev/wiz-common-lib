22b1de364297a06f6b6e17e10270b845e3d5dfd9
shhan <shhan@softwiz.co.kr>
2025-11-01 12:21:37 +0900

    dd

---
+++ src/main/java/org/softwiz/platform/iot/common/lib/dto/PageRequest.java
@@ -0,0 +1,38 @@
+package org.softwiz.platform.iot.common.lib.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PageRequest {
+    
+    @Builder.Default
+    private Integer page = 1;
+    
+    @Builder.Default
+    private Integer pageSize = 20;
+    
+    private Integer offset;
+    
+    public void init() {
+        if (this.page == null || this.page < 1) {
+            this.page = 1;
+        }
+        if (this.pageSize == null || this.pageSize < 1) {
+            this.pageSize = 20;
+        }
+        this.offset = (this.page - 1) * this.pageSize;
+    }
+    
+    public Integer getOffset() {
+        if (this.offset == null) {
+            init();
+        }
+        return this.offset;
+    }
+}
